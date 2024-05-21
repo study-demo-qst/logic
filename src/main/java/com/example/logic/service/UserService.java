363fb70405be66d6e2aec8f98ce21f33cbe112a7
zuruzirou <zuruzirou@gmail.com>
2024-05-21 23:39:56 +0900

    a

---
+++ src/main/java/com/example/logic/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.logic.service;
+
+import com.example.common.entity.User;
+import com.example.common.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
