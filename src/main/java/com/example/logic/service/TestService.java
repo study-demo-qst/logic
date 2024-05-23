d5d83ebf40f94362030f96f3880ee6257c133d2d
zuruzirou <zuruzirou@gmail.com>
2024-05-23 09:24:54 +0900

    a

--- src/main/java/com/example/logic/service/TestService.java
+++
@@ -1,5 +0,0 @@
-package com.example.logic.service;
-
-public class TestService {
-
-}
--- src/main/java/com/example/logic/service/UserService.java
+++ src/main/java/com/example/logic/service/UserService.java
@@ -1,10 +1,11 @@
 package com.example.logic.service;
 
-import com.example.common.entity.User;
+import com.example.common.model.User;
 import com.example.common.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,15 +14,19 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public Optional<User> findUserByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 
-    public Optional<User> findUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
     }
 
-    public User saveUser(User user) {
+    public User save(User user) {
         return userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
     }
 }
